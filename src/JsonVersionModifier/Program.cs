5f45cbee29c8105ddde3247eb6ac2ad0f6a8fcc4
N. Taylor Mullen <nimullen@microsoft.com>
2016-01-20 22:03:43 -0800

    Initial commit.

---
+++ src/JsonVersionModifier/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace JsonVersionModifier
+{
+    public class Program
+    {
+        private static readonly HashSet<string> PostRTMPackages = new HashSet<string>(StringComparer.Ordinal)
+        {
+            "Microsoft.AspNet.Authentication.OpenIdConnect",
+            "Microsoft.AspNet.Buffering",
+            "Microsoft.AspNet.Diagnostics.Elm",
+            "Microsoft.AspNet.HttpOverrides",
+            "Microsoft.AspNet.Proxy",
+            "Microsoft.AspNet.Server.WebListener",
+            "Microsoft.AspNet.SignalR.Messaging",
+            "Microsoft.AspNet.SignalR.Redis",
+            "Microsoft.AspNet.SignalR.Server",
+            "Microsoft.AspNet.SignalR.Sources",
+            "Microsoft.AspNet.SignalR.SqlServer",
+            "Microsoft.AspNet.WebSockets.Client",
+            "Microsoft.AspNet.WebSockets.Protocol",
+            "Microsoft.AspNet.WebSockets.Server",
+        };
+
+        public static void Main(string[] args)
+        {
+            foreach (var fileName in args)
+            {
+                var projectName = Path.GetDirectoryName(fileName).Split('/', '\\').Last();
+                using (var file = File.OpenText(fileName))
+                using (var reader = new JsonTextReader(file))
+                {
+                    var data = (JObject)JToken.ReadFrom(reader);
+                    var newVersion = PostRTMPackages.Contains(projectName) ? "0.1-*" : "1.0-*";
+                    data["version"] = newVersion;
+
+                    foreach (var dependency in ((JObject)data["dependencies"]).Properties())
+                    {
+                        if (PostRTMPackages.Contains(dependency.Name))
+                        {
+                            SetVersion("0.1.0-*", dependency);
+                        }
+                        else
+                        {
+                            var version = GetVersion(dependency);
+
+                            if (dependency.Name.StartsWith("System", StringComparison.Ordinal) ||
+                                version.StartsWith("1.0.0", StringComparison.Ordinal))
+                            {
+                                continue;
+                            }
+                            else if (dependency.Name.Contains("AspNet"))
+                            {
+                                SetVersion("1.0.0-*", dependency);
+                            }
+                            else
+                            {
+                                Console.WriteLine(dependency.Name + ": " + version);
+                                Console.WriteLine("Force to 1.0.0?");
+                                var result = Console.ReadLine();
+                                if (result[0] == 'y' || result[0] == 'Y')
+                                {
+                                    SetVersion("1.0.0", dependency);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void SetVersion(string version, JProperty dependency)
+        {
+            if (dependency.HasValues)
+            {
+                dependency["version"] = version;
+            }
+            else
+            {
+                dependency.Value = version;
+            }
+        }
+
+        private static string GetVersion(JProperty dependency)
+        {
+            if (dependency.HasValues)
+            {
+                return dependency["version"].Value<string>();
+            }
+            else
+            {
+                return dependency.Value<string>();
+            }
+        }
+
+        private static bool IsV1(JProperty dependency)
+        {
+            if (dependency.HasValues)
+            {
+                return dependency["version"].Value<string>().StartsWith("1.0.0");
+            }
+            else
+            {
+                return dependency.Value<string>().StartsWith("1.0.0");
+            }
+        }
+    }
+}
---
+++ src/JsonVersionModifier/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("JsonVersionModifier")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("JsonVersionModifier")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1f5b4740-3162-41f2-b0f6-1b95d554875c")]
