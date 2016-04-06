63b29851c1a1e258755700608f50e91dd8eaf4c4
Pavel Krymets <pavel@krymets.com>
2016-04-06 15:26:16 -0700

    Update to portable

--- src/JsonVersionModifier/Program.cs
+++ src/JsonVersionModifier/Program.cs
@@ -11,42 +11,12 @@ namespace JsonVersionModifier
 {
     public class Program
     {
-        private static readonly HashSet<string> PostRTMPackages = new HashSet<string>(StringComparer.Ordinal)
-        {
-            "Microsoft.AspNetCore.Authentication.OpenIdConnect",
-            "Microsoft.AspNetCore.Buffering",
-            "Microsoft.AspNetCore.Diagnostics.Elm",
-            "Microsoft.AspNetCore.HttpOverrides",
-            "Microsoft.AspNetCore.Proxy",
-            "Microsoft.AspNetCore.Server.WebListener",
-            "Microsoft.AspNetCore.SignalR.Messaging",
-            "Microsoft.AspNetCore.SignalR.Redis",
-            "Microsoft.AspNetCore.SignalR.Server",
-            "Microsoft.AspNetCore.SignalR.Sources",
-            "Microsoft.AspNetCore.SignalR.SqlServer",
-            "Microsoft.AspNetCore.SignalR.ServiceBus",
-            "Microsoft.AspNetCore.WebSockets.Client",
-            "Microsoft.AspNetCore.WebSockets.Protocol",
-            "Microsoft.AspNetCore.WebSockets.Server",
-            "Microsoft.Net.Http.Server",
-            "Microsoft.Net.WebSockets.Server",
-        };
         private static Dictionary<string, bool> _packageLookups = new Dictionary<string, bool>(StringComparer.Ordinal);
 
         public static void Main(string[] args)
         {
             try
             {
-                Console.WriteLine("Starting Version Updates...");
-
-                using (var file = File.OpenText("C:/Users/nimullen/Documents/GitHub/JsonVersionModifier/src/JsonVersionModifier/packageLookups.json"))
-                using (var reader = new JsonTextReader(file))
-                {
-                    var serializer = new JsonSerializer();
-                    _packageLookups = serializer.Deserialize<Dictionary<string, bool>>(reader);
-                }
-                Console.WriteLine("Package Lookup cache loaded.");
-
                 Console.WriteLine();
                 Console.WriteLine("Updating versions for: " + Environment.NewLine + string.Join(Environment.NewLine, args));
                 Console.WriteLine();
@@ -65,66 +35,15 @@ namespace JsonVersionModifier
                         {
                             data = (JObject)JToken.ReadFrom(reader);
                         }
-                        catch
+                        catch (Exception ex)
                         {
-                            Console.WriteLine("Invalid project.json");
+                            Console.WriteLine("Invalid project.json" + ex);
                             return;
                         }
 
-                        var projectVersion = data["version"];
-
-                        if (projectVersion != null)
-                        {
-                            var projectVersionValue = projectVersion.Value<string>();
-
-                            if (PostRTMPackages.Contains(projectName))
-                            {
-                                updateFile = true;
-                                data["version"] = "0.1.0-*";
-                            }
-                            else if (!projectVersionValue.StartsWith("1.0.0", StringComparison.Ordinal))
-                            {
-                                if (projectName.Contains("AspNetCore") ||
-                                    _packageLookups.ContainsKey(projectName) &&
-                                    _packageLookups[projectName])
-                                {
-                                    updateFile = true;
-                                    data["version"] = "1.0.0-*";
-                                }
-                                else if (!_packageLookups.ContainsKey(projectName))
-                                {
-                                    Console.WriteLine(projectName + ": " + projectVersionValue);
-                                    Console.WriteLine("Force to 1.0.0-*?");
-
-                                    var result = Console.ReadLine();
-                                    if (result[0] == 'y' || result[0] == 'Y')
-                                    {
-                                        updateFile = true;
-                                        _packageLookups[projectName] = true;
-                                        data["version"] = "1.0.0-*";
-                                    }
-                                    else
-                                    {
-                                        _packageLookups[projectName] = false;
-                                    }
-                                }
-                            }
-                        }
-
-                        if (data["dependencies"] != null && data["dependencies"] is JObject)
-                        {
-                            updateFile = UpdateChildVersions(((JObject)data["dependencies"]).Properties()) || updateFile;
-                        }
-
-                        var children = data["frameworks"].Children();
-                        foreach (var framework in children)
-                        {
-                            var frameworkDependencies = framework.Values("dependencies");
-                            if (frameworkDependencies != null)
-                            {
-                                updateFile = UpdateChildVersions(frameworkDependencies.Values<JProperty>()) || updateFile;
-                            }
-                        }
+                        updateFile |= UpdateDependencies(data);
+                        updateFile |= UpdateFrameworks(data);
+                        updateFile |= AddRuntimes(data);
                     }
 
                     if (updateFile)
@@ -138,94 +57,65 @@ namespace JsonVersionModifier
                 Console.WriteLine("Error: " + ex.ToString());
                 Debugger.Launch();
             }
-            finally
-            {
-                SaveDataTo("C:/Users/nimullen/Documents/GitHub/JsonVersionModifier/src/JsonVersionModifier/packageLookups.json", _packageLookups);
-            }
         }
 
-        private static bool UpdateChildVersions(IEnumerable<JProperty> dependencies)
+        private static bool UpdateDependencies(JToken data)
         {
-            var updateFile = false;
-            foreach (var dependency in dependencies)
+            var dependencies = data["dependencies"] as JObject;
+            if (dependencies == null)
             {
-                if (PostRTMPackages.Contains(dependency.Name))
+                return false;
+            }
+
+            var updateFile = false;
+            foreach (var dependency in dependencies.Properties().ToArray())
+            {
+                if (dependency.Name == "NETStandard.Library")
                 {
-                    SetVersion("0.1.0-*", dependency);
+                    dependency.Replace(new JProperty("Microsoft.NETCore.App",
+                        new JObject(
+                            new JProperty("version", "1.0.0-*"),
+                            new JProperty("type", "platform"))));
                     updateFile = true;
-                }
-                else
-                {
-                    var version = GetVersion(dependency);
-
-                    if (dependency.Name.StartsWith("System", StringComparison.Ordinal) ||
-                        version.StartsWith("1.0.0", StringComparison.Ordinal))
-                    {
-                        continue;
-                    }
-                    else if (dependency.Name.Contains("AspNetCore") ||
-                        _packageLookups.ContainsKey(dependency.Name) &&
-                        _packageLookups[dependency.Name])
-                    {
-                        updateFile = true;
-                        SetVersion("1.0.0-*", dependency);
-                    }
-                    else if (!_packageLookups.ContainsKey(dependency.Name))
-                    {
-                        Console.WriteLine(dependency.Name + ": " + version);
-                        Console.WriteLine("Force to 1.0.0-*");
-
-                        var result = Console.ReadLine();
-                        if (result[0] == 'y' || result[0] == 'Y')
-                        {
-                            Console.WriteLine("Forcing to 1.0.0.");
-                            updateFile = true;
-                            _packageLookups[dependency.Name] = true;
-                            SetVersion("1.0.0-*", dependency);
-                        }
-                        else
-                        {
-                            _packageLookups[dependency.Name] = false;
-                        }
-                    }
                 }
             }
 
             return updateFile;
         }
 
-        private static void SetVersion(string version, JProperty dependency)
+        private static bool UpdateFrameworks(JToken data)
         {
-            var child = dependency.Children().First();
-            if (child.HasValues)
+            var frameworks = data["frameworks"] as JObject;
+            var updateFile = false;
+            foreach (var framework in frameworks.Properties().ToArray())
             {
-                child["version"] = version;
+                if (framework.Name.StartsWith("netstandard"))
+                {
+                    UpdateDependencies(framework.Value);
+                    framework.Replace(new JProperty("netcoreapp1.0", framework.Value));
+                    updateFile = true;
+                }
             }
-            else
-            {
-                dependency.Value = version;
-            }
+            return updateFile;
         }
 
-        private static string GetVersion(JProperty dependency)
+        private static bool AddRuntimes(JToken data)
         {
-            var child = dependency.Children().First();
+            var frameworks = data["frameworks"] as JObject;
 
-            if (child.HasValues && child["version"] != null)
+            var updateFile = false;
+            if (frameworks.Properties().Any(p => p.Name.StartsWith("net")))
             {
-                return child["version"].Value<string>();
-            }
-            else
-            {
-                try
+                var runtimes = data["runtimes"] as JObject;
+                if (runtimes == null)
                 {
-                    return child.Value<string>();
+                    data["runtimes"] = (runtimes = new JObject());
                 }
-                catch
-                {
-                    return string.Empty;
-                }
+                runtimes["win7-x64"] = new JObject();
+                runtimes["win7-x86"] = new JObject();
+                updateFile = true;
             }
+            return updateFile;
         }
 
         private static void SaveDataTo(string fileName, object data)
---
+++ src/JsonVersionModifier/update.ps1
@@ -0,0 +1,15 @@
+git checkout .
+
+if (test-path test)
+{
+    $projects = (Get-ChildItem -Path test -Filter project.json -Recurse | %{ $_.FullName })
+}
+if (test-path samples)
+{
+    $projects = $projects + (Get-ChildItem -Path samples -Filter project.json -Recurse | %{ $_.FullName })
+}
+
+$projects| Write-Host
+dotnet run -p $PSScriptRoot -- $projects
+
+.\build
